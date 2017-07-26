094b36e59c71ccce793d5c1141688627cec0962d
Shih-Po Wang <shihpo@gmail.com>
2017-07-26 11:21:37 +0800

    add CanvasView

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-class ShoppingList extends React.Component {
-  render() {
-    return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
-    );
-  }
-}
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
