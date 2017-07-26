5fb582409eab489f69f9264a0f6baca64d4b8b34
Shih-Po Wang <shihpo@gmail.com>
2017-07-26 12:00:13 +0800

    setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname = "/src",
+  entry: "./index.js",
+
+  output: {
+    filename: "index.js",
+    path: __dirname + "dist"
+  }
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          __dirname + "/src"
+        ],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ]
+  }
+}
